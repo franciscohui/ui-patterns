f83dfea82f75b4526c4600402b9f7ac1e2f5d917
Francisco Hui <f@franciscohui.com>
2013-08-19 22:04:40 -0400

    Add slide toggle to nav menu

--- js/general.js
+++ js/general.js
@@ -3,10 +3,16 @@ $(document).ready(function(){
 	//Load content
 	$('#content').load('content/about.html');
 
+	//Click Nav to display content
+	//$('nav a').click(function(){
+	//	$('input#nav-handler').attr('checked',false);
+	//});
+
 	//handle menu clicks
 	$('nav a').click(function(){
 		var page = $(this).attr('href');
 		$('#content').load('content/'+ page + '.html');
+		$('input#nav-handler').attr('checked',false);
 		return false;
 	});
 });
