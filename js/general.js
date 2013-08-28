cf298c1be6abc131cb4accba54205f30bcdad63c
Francisco Hui <f@franciscohui.com>
2013-08-28 19:45:52 -0400

    Create Sidenotes with localStorage

--- js/general.js
+++ js/general.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
 	
 	//Load content
-	$('#content').load('content/about.html');
+	$('#content').load('content/home.html');
 
 	//Click Nav to display content
 	//$('nav a').click(function(){
@@ -11,8 +11,8 @@ $(document).ready(function(){
 	//handle menu clicks
 	$('nav a').click(function(){
 		var page = $(this).attr('href');
-		$('#content').load('content/'+ page + '.html');
-		$('input#nav-handler').attr('checked',false);
+		$('#content').load('content/'+ page + '.html'); // Loads selected page content
+		$('input#nav-handler').attr('checked',false); //Toggles view slide on click
 		return false;
 	});
 });
